956b80d8edc71ddbd40c5fc002b10904e6beb2eb
Ryah <ryadame@my.lonestar.edu>
2020-12-17 19:18:17 -0600

    change main.js

--- main.user.js
+++ main.user.js
@@ -2,12 +2,12 @@
 // @name            Rosie Addons Suite
 // @namespace       http://ryah.org/
 // @match           https://retailers.rosieapp.com/*
-// @version         1.51
+// @version         1.52
 // @description     Addons for Rosie Retailers because the site needs improvement
 // @author          Ryan Adame
 // @require         https://cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require         https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/js/waitForKeyElements_offline.js?token=ADSLCWNO4EKWE5GKXMDXQMC74UTFG
-// @require         https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/js/Rosie%20Addon%20Suite.js?token=ADSLCWJ2FQK2QV3LQI6KJ2S74U2XW
+// @require         https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/js/Rosie%20Addon%20Suite.js?token=ADSLCWMHWFB63QDNJWRP6TK74U6YU
 // @resource        payCalcHTML https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/html/paymentCalculator.html?token=ADSLCWL7MXMCSKXBEX35OEK74UTSW
 // @resource        customCSS https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/css/styles.css?token=ADSLCWMBPA5QCOOABJR2XQS74UTUM
 // @resource        orderHistBut https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/html/orderHist.html?token=ADSLCWMCUTS2FYNUYNRGVES74UWNC
