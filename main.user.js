4245fac0a0373fdbf869ac48288bc33d29f6efbe
Ryah <sean@DESKTOP-NBQTKU9.localdomain>
2020-12-17 18:43:18 -0600

    File Structure 2/2

---
+++ main.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name            Rosie Addons Suite
+// @namespace       http://ryah.org/
+// @match           https://retailers.rosieapp.com/*
+// @version         1.5
+// @description     Addons for Rosie Retailers because the site needs improvement
+// @author          Ryan Adame
+// @require         https://cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @require         https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/js/waitForKeyElements_offline.js?token=ADSLCWNO4EKWE5GKXMDXQMC74UTFG
+// @require         file://C:/Rosie-Addon-Suite/js/Rosie Addon Suite.js
+// @resource        payCalcHTML https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/html/paymentCalculator.html?token=ADSLCWL7MXMCSKXBEX35OEK74UTSW
+// @resource        customCSS https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/css/styles.css?token=ADSLCWMBPA5QCOOABJR2XQS74UTUM
+// @resource        orderHistBut https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/html/orderHist.html?token=ADSLCWMCUTS2FYNUYNRGVES74UWNC
+// @grant           GM_addStyle
+// @grant           GM_getResourceText
+// ==/UserScript==
