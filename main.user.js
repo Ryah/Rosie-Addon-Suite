68823f8d3688f4ff95dc4a349d9cef7a8266dac6
Ryah <sean@DESKTOP-NBQTKU9.localdomain>
2020-12-17 18:44:48 -0600

    File Structure 3/2

--- main.user.js
+++ main.user.js
@@ -7,7 +7,7 @@
 // @author          Ryan Adame
 // @require         https://cdnjs.cloudflare.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // @require         https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/js/waitForKeyElements_offline.js?token=ADSLCWNO4EKWE5GKXMDXQMC74UTFG
-// @require         file://C:/Rosie-Addon-Suite/js/Rosie Addon Suite.js
+// @require         https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/js/Rosie%20Addon%20Suite.js?token=ADSLCWJ2FQK2QV3LQI6KJ2S74U2XW
 // @resource        payCalcHTML https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/html/paymentCalculator.html?token=ADSLCWL7MXMCSKXBEX35OEK74UTSW
 // @resource        customCSS https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/css/styles.css?token=ADSLCWMBPA5QCOOABJR2XQS74UTUM
 // @resource        orderHistBut https://raw.githubusercontent.com/Ryah/Rosie-Addon-Suite/main/html/orderHist.html?token=ADSLCWMCUTS2FYNUYNRGVES74UWNC
